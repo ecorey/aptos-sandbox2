a8b0476cd8771728f8686fb694fa1276fbb0565d
ecorey <bengstrom32@gmail.com>
2024-07-26 18:11:40 -0400

    commit

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -24,7 +24,7 @@ function App() {
           </Card>
         ) : (
           <CardHeader>
-            <CardTitle>To get started Connect a wallet</CardTitle>
+            <CardTitle>Aptos Sandbox</CardTitle>
           </CardHeader>
         )}
       </div>
