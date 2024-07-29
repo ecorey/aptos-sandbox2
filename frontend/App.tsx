844645b588e5bc04015118d7a9cbe0693417c449
ecorey <bengstrom32@gmail.com>
2024-07-29 11:33:08 -0400

    commit

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -24,7 +24,7 @@ function App() {
           </Card>
         ) : (
           <CardHeader>
-            <CardTitle>Aptos Sandbox</CardTitle>
+            <CardTitle>Sandbox</CardTitle>
           </CardHeader>
         )}
       </div>
